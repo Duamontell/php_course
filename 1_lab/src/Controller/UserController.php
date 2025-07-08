585a9e8dbec23f0416bc240055ecf4fc91f6b6c4
Dutell <duamontell@gmail.com>
2025-07-08 21:19:38 +0300

    Create and setting image service

--- 1_lab/src/Controller/UserController.php
+++ 1_lab/src/Controller/UserController.php
@@ -6,15 +6,18 @@ namespace App\Controller;
 
 use App\Model\UserTable;
 use App\Model\Entity\User;
-use RuntimeException;
+use App\Service\ImageService;
+use Ramsey\Uuid\Uuid;
 
 class UserController
 {
 	private UserTable $userTable;
+	private ImageService $imageService;
 
 	public function __construct(\PDO $pdo)
 	{
 		$this->userTable = new UserTable($pdo);
+		$this->imageService = new ImageService();
 	}
 
 	public function index()
@@ -96,19 +99,14 @@ class UserController
 				throw new \RuntimeException("Обязательные поля должны быть заполнены!");
 			}
 
-			$params["avatar_path"] = null;
-
-			$avatar = $this->getUserAvatar();
+			if (is_uploaded_file($_FILES["avatar"]["tmp_name"])) {
+				$params["avatar_path"] = $this->imageService->saveUserAvatar($_FILES["avatar"]);
+			} else {
+				$params["avatar_path"] = null;
+			}
 
 			$user = User::createUserFromParams(null, $params);
 			$id = $this->getUserTable()->saveUserToDatabase($this->getUserTable()->getPDO(), $user);
-
-			if ($avatar != null) {
-				// Переместить в ImageService
-				$newAvatarName = $this->generateAvatarFilename($id, $avatar["avatarExtension"]);
-				$this->moveUserAvatar($_FILES["avatar"]["tmp_name"], $newAvatarName);
-				$this->getUserTable()->updateAvatarPathInDatabase($this->getUserTable()->getPDO(), $id, $newAvatarName);
-			}
 
 			$redirectUrl = "?action=profile&user_id=$id";
 			header("Location: " . $redirectUrl, true, 303);
@@ -128,27 +126,24 @@ class UserController
 				throw new \RuntimeException("Обязательные поля должны быть заполнены и/или не превышать лимит символов!");
 			}
 
-			$avatar = $this->getUserAvatar();
-
 			if (is_null($user = $this->getUserTable()->findUserInDatabase($this->getUserTable()->getPDO(), $userId))) {
 				$this->redirectWithError("Такого пользователя не существует!");
 			}
 
-			$params["avatar_path"] = $user->getAvatarPath();
+			if (is_uploaded_file($_FILES["avatar"]["tmp_name"])) {
+				$params["avatar_path"] = $this->imageService->saveUserAvatar($_FILES["avatar"]);
+			} else {
+				$params["avatar_path"] = $user->getAvatarPath();
+			}
+
 			$updatedUser = User::createUserFromParams($userId, $params);
 
 			$this->getUserTable()->updateUserInDatabase($this->getUserTable()->getPDO(), $updatedUser);
 
-			if ($avatar != null) {
-				$newAvatarName = $this->generateAvatarFilename($userId, $avatar["avatarExtension"]);
-				$this->moveUserAvatar($_FILES["avatar"]["tmp_name"], $newAvatarName);
-				$this->getUserTable()->updateAvatarPathInDatabase($this->getUserTable()->getPDO(), $userId, $newAvatarName);
-			}
-
 			$redirectUrl = "?action=profile&user_id=$userId";
 			header("Location: " . $redirectUrl, true, 303);
 			die();
-		} catch (\PDOException) {
+		} catch (\PDOException $e) {
 			$this->redirectWithError("Пользователь с таким email или номером телефона уже сущестует");
 		} catch (\RuntimeException $e) {
 			$this->redirectWithError($e->getMessage());
@@ -178,7 +173,6 @@ class UserController
 			$ar["middle_name"] = null;
 		}
 		if (empty($ar["phone"]) || $ar["phone"] == "") {
-			echo ("DELAEM NULL ");
 			$ar["phone"] = null;
 		};
 
@@ -190,44 +184,6 @@ class UserController
 			&& !empty($ar["email"]) && strlen($ar["email"]) <= 75;
 	}
 
-	private function getUserAvatar(): ?array
-	{
-		$avatar = [
-			"avatarPath" => null,
-			"avatarExtension" => null
-		];
-
-		if (is_uploaded_file($_FILES["avatar"]["tmp_name"])) {
-			$avatar["avatarPath"] = "avatar";
-			$avatar["avatarExtension"] = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
-			if (!$this->checkFileExtensions($avatar["avatarExtension"])) {
-				throw new RuntimeException("Недопустимый формат аватара!");
-			}
-
-			return $avatar;
-		}
-
-		return null;
-	}
-
-	private function generateAvatarFilename(int $userId, string $extension): string
-	{
-		return "avatar{$userId}" . "." . $extension;
-	}
-
-	private function moveUserAvatar(string $path, string $filename)
-	{
-		$uploadDir = "uploads/" . $filename;
-		if (!move_uploaded_file($path, $uploadDir)) {
-			throw new \RuntimeException("Ошибка сохранении аватара!");
-		}
-	}
-
-	private static function checkFileExtensions(string $ext): bool
-	{
-		return in_array($ext, ["png", "jpeg", "gif"]);
-	}
-
 	public static function redirectWithError(string $message)
 	{
 		$redirectUrl = "?action=error&msg=" . $message;
@@ -235,7 +191,7 @@ class UserController
 		die();
 	}
 
-	public function getUserTable(): UserTable
+	private function getUserTable(): UserTable
 	{
 		return $this->userTable;
 	}
--- 1_lab/vendor/composer/autoload_static.php
+++ 1_lab/vendor/composer/autoload_static.php
@@ -6,7 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit35ba2b49ad72605fe7240453613a0e6d
 {
+    public static $files = array (
+        'e39a8b23c42d4e1452234d762b03835a' => __DIR__ . '/..' . '/ramsey/uuid/src/functions.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'R' => 
+        array (
+            'Ramsey\\Uuid\\' => 12,
+            'Ramsey\\Collection\\' => 18,
+        ),
+        'B' => 
+        array (
+            'Brick\\Math\\' => 11,
+        ),
         'A' => 
         array (
             'App\\' => 4,
@@ -14,6 +27,18 @@ class ComposerStaticInit35ba2b49ad72605fe7240453613a0e6d
     );
 
     public static $prefixDirsPsr4 = array (
+        'Ramsey\\Uuid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/ramsey/uuid/src',
+        ),
+        'Ramsey\\Collection\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/ramsey/collection/src',
+        ),
+        'Brick\\Math\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/brick/math/src',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
