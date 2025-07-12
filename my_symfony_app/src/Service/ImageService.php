8f7be2e93034bab2f1f844607a875ca37be442de
Dutell <duamontell@gmail.com>
2025-07-12 17:17:46 +0300

    Refactor image service

--- my_symfony_app/src/Service/ImageService.php
+++ my_symfony_app/src/Service/ImageService.php
@@ -8,51 +8,51 @@ use Ramsey\Uuid\Uuid;
 
 class ImageService
 {
-	private array $allowedExtensions = ["jpeg", "png", "gif"];
+    private array $allowedExtensions = ["jpeg", "png", "gif"];
 
-	public function saveImage(array $file): string
-	{
-		if ($file['error'] !== UPLOAD_ERR_OK) {
-			throw new \RuntimeException('Ошибка при загрузке файла.');
-		}
+    public function saveImage(array $file): string
+    {
+        if ($file['error'] !== UPLOAD_ERR_OK) {
+            throw new \RuntimeException('Ошибка при загрузке файла.');
+        }
 
-		$avatarExtension = pathinfo($file["name"], PATHINFO_EXTENSION);
-		if (!$this->checkFileExtensions($avatarExtension)) {
-			throw new \RuntimeException("Недопустимый формат аватара");
-		}
+        $avatarExtension = pathinfo($file["name"], PATHINFO_EXTENSION);
+        if (!$this->checkFileExtensions($avatarExtension)) {
+            throw new \RuntimeException("Недопустимый формат изображения");
+        }
 
-		$newAvatarName = $this->generateAvatarFilename($avatarExtension);
-		$this->moveUserImage($file["tmp_name"], $newAvatarName);
+        $newAvatarName = $this->generateAvatarFilename($avatarExtension);
+        $this->moveUserImage($file["tmp_name"], $newAvatarName);
 
-		return $newAvatarName;
-	}
+        return $newAvatarName;
+    }
 
-	public function deleteImage(string $filePath): bool
-	{
-		$destination = "uploads/" . $filePath;
-		if (!file_exists($destination)) {
-			throw new \RuntimeException("Удаляемый файл не найден!");
-		}
-		
-		return unlink($destination);
-	}
+    public function deleteImage(string $filePath): bool
+    {
+        $destination = "uploads/" . $filePath;
+        if (!file_exists($destination)) {
+            throw new \RuntimeException("Удаляемый файл не найден!");
+        }
 
-	private function checkFileExtensions(string $ext): bool
-	{
-		return in_array($ext, $this->allowedExtensions);
-	}
+        return unlink($destination);
+    }
 
-	private function generateAvatarFilename(string $extension): string
-	{
-		$randomUuid = Uuid::uuid4();
-		return "avatar{$randomUuid}" . "." . $extension;
-	}
+    private function checkFileExtensions(string $ext): bool
+    {
+        return in_array($ext, $this->allowedExtensions);
+    }
 
-	private function moveUserImage(string $path, string $filename)
-	{
-		$uploadDir = "uploads/" . $filename;
-		if (!move_uploaded_file($path, $uploadDir)) {
-			throw new \RuntimeException("Ошибка сохранении аватара!");
-		}
-	}
+    private function generateAvatarFilename(string $extension): string
+    {
+        $randomUuid = Uuid::uuid4();
+        return "avatar{$randomUuid}" . "." . $extension;
+    }
+
+    private function moveUserImage(string $path, string $filename)
+    {
+        $uploadDir = "uploads/" . $filename;
+        if (!move_uploaded_file($path, $uploadDir)) {
+            throw new \RuntimeException("Ошибка сохранении изображения!");
+        }
+    }
 }
