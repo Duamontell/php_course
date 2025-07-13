4288d1f64dadeff7980dcddbdfd95320cc5ae6a6
Dutell <duamontell@gmail.com>
2025-07-14 00:46:21 +0300

    feat: Added register user and login

--- my_symfony_app/src/Service/PasswordHasher.php
+++ my_symfony_app/src/Service/PasswordHasher.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Service;
@@ -15,28 +16,46 @@ class PasswordHasher implements PasswordHasherInterface
 
     public function hash(string $plainPassword): string
     {
-        if ($this->isPasswordTooLong($plainPassword))
-        {
-            throw new InvalidPasswordException();
-        }
-
-        return $this->encodePassword($plainPassword);
+        return password_hash($plainPassword, PASSWORD_DEFAULT);
     }
+
+    // public function hash(string $plainPassword): string
+    // {
+    //     if ($this->isPasswordTooLong($plainPassword))
+    //     {
+    //         throw new InvalidPasswordException();
+    //     }
+
+    //     return $this->encodePassword($plainPassword);
+    // }
 
     public function verify(string $hashedPassword, string $plainPassword): bool
     {
-        if ('' === $plainPassword || $this->isPasswordTooLong($plainPassword))
-        {
+        if ('' === $plainPassword || $this->isPasswordTooLong($plainPassword)) {
             return false;
         }
-
-        return $this->encodePassword($plainPassword) === $hashedPassword;
+        return password_verify($plainPassword, $hashedPassword);
     }
+
+    // public function verify(string $hashedPassword, string $plainPassword): bool
+    // {
+    //     if ('' === $plainPassword || $this->isPasswordTooLong($plainPassword))
+    //     {
+    //         return false;
+    //     }
+
+    //     return $this->encodePassword($plainPassword) === $hashedPassword;
+    // }
 
     public function needsRehash(string $hashedPassword): bool
     {
-        return false;
+        return password_needs_rehash($hashedPassword, PASSWORD_DEFAULT);
     }
+
+    // public function needsRehash(string $hashedPassword): bool
+    // {
+    //     return false;
+    // }
 
     private function encodePassword(string $password): string
     {
